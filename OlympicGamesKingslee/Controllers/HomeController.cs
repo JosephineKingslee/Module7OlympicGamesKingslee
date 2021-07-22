0a25461244f488d9c75848825ed758285d9c4f06
Kingslee1604 <47510140+Kingslee1604@users.noreply.github.com>
2021-07-21 20:45:45 -0500

    Module7OlympicGamesKingslee_Final

--- OlympicGamesKingslee/Controllers/HomeController.cs
+++ OlympicGamesKingslee/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using OlympicGamesKingslee.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace OlympicGamesKingslee.Controllers
 {
@@ -16,24 +17,20 @@ namespace OlympicGamesKingslee.Controllers
             context = ctx;
         }
 
-        public ViewResult Index(string activeCategory = "all", string activeGameType = "all")
+        public IActionResult Index(string activeCategory = "all", string activeGameType = "all")
         {
-            ViewBag.ActiveCategory = activeCategory;
-            ViewBag.ActiveGameType = activeGameType;
+            var session = new OlympicsSession(HttpContext.Session);
+            session.SetActiveCategory(activeCategory);
+            session.SetActiveGameType(activeGameType);
 
-            // get list of categories and game types from database
-            List<Category> categories = context.Categories.ToList();
-            List<GameType> gameTypes = context.GameTypes.ToList();
+            var model = new OlympicsListViewModel
+            {
+                ActiveCategory = activeCategory,
+                ActiveGameType = activeGameType,
+                Categories = context.Categories.ToList(),
+                GameTypes = context.GameTypes.ToList()
+            };
 
-            // insert default "All" value at front of each list
-            categories.Insert(0, new Category { CategoryID = "all", Name = "All" });
-            gameTypes.Insert(0, new GameType { GameTypeID = "all", Name = "All" });
-
-            // store lists in view bag
-            ViewBag.Categories = categories;
-            ViewBag.GameTypes = gameTypes;
-
-            // get olympics - filter by categories and game types
             IQueryable<Olympics> query = context.Olympics;
             if (activeCategory != "all")
                 query = query.Where(
@@ -41,11 +38,49 @@ namespace OlympicGamesKingslee.Controllers
             if (activeGameType != "all")
                 query = query.Where(
                     t => t.GameType.GameTypeID.ToLower() == activeGameType.ToLower());
+            model.Olympics = query.ToList();
 
-            // pass olympics to view as model
-            var olympics = query.ToList();
-            return View(olympics);
-
+            return View(model);
         }
+
+        public IActionResult Details(string id)
+        {
+            var session = new OlympicsSession(HttpContext.Session);
+            var model = new OlympicsViewModel
+            {
+                olympics = context.Olympics
+                    .Include(t => t.Category)
+                    .Include(t => t.GameType)
+                    .FirstOrDefault(t => t.CountryID == id),
+                ActiveGameType = session.GetActiveGameType(),
+                ActiveCategory = session.GetActiveCategory()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Add(OlympicsViewModel data)
+        {
+            data.olympics = context.Olympics
+                .Include(t => t.Category)
+                .Include(t => t.GameType)
+                .Where(t => t.CountryID == data.olympics.CountryID)
+                .FirstOrDefault();
+
+            var session = new OlympicsSession(HttpContext.Session);
+            var olympics = session.GetMyOlympics();
+            olympics.Add(data.olympics);
+            session.SetMyOlympics(olympics);
+
+            TempData["message"] = $"{data.olympics.Name} added to your favorites";
+
+            return RedirectToAction("Index",
+                new
+                {
+                    ActiveCategory = session.GetActiveCategory(),
+                    ActiveGameType = session.GetActiveGameType()
+                });
+        }
+
     }
 }
